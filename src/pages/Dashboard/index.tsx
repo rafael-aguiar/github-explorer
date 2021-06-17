c9ffe177d1e7961aee55065e3e6e0127a5164c41
Rafael Aguiar <rafael.nascimento.aguiar@outlook.com>
2021-06-17 20:07:01 -0300

    add routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <h1>Olá, mundo! Este é o Dashboard.</h1>
+  );
+}
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return (
+    <h1>Olá, mundo! Este é o Repository.</h1>
+  );
+}
+
+export default Repository;
