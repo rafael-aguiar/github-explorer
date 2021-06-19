b7b568296012aee9a31861eb6004c89dc0b4a31e
Rafael Aguiar <rafael.nascimento.aguiar@outlook.com>
2021-06-19 15:34:11 -0300

    add repositories styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Header, Issues, RepositoryInfo } from './styles';
 
 interface repositoryParams {
   repository: string;
@@ -7,7 +12,51 @@ interface repositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<repositoryParams>();
-  return <h1>Olá, mundo! Este é o Repositório {params.repository}.</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer " />
+        <Link to="/">
+          <FiChevronLeft size={18} />
+          voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>Repo name</strong>
+            <p>repo info</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>42</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>8</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>2</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link key="{}" to="/">
+          <div>
+            <strong>{}</strong>
+            <p>{}</p>
+          </div>
+          <FiChevronRight size={24} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
